6c969b8e5fc5156eea61dc93bb7144229e91365a
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2024-02-05 11:13:02 -0600

    fix linter errors

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require_relative 'modules/on_start'
 require_relative 'modules/exit_app'
-require_relative  'modules/create_csv'
+require_relative 'modules/create_csv'
 require_relative 'modules/download_pdf'
 
 class App
